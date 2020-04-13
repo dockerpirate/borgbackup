4aad2a37e0251f98afbd77ebc2ecb9868c027605
dockerpirate <52665720+dockerpirate@users.noreply.github.com>
2020-04-13 17:37:32 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,6 @@ mkdir /etc/periodic/hourly && \
 mkdir /etc/periodic/monthly && \
 mkdir /etc/periodic/weekly && \
 mv /borg_auto /etc/periodic/hourly/ && \ 
+chmod +x /etc/periodic/hourly/borg_auto && \
 crond -b -c /etc/crontabs/ -L /tmp/cron-log && \
 /usr/bin/supervisord
